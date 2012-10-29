d617760b46018fad02edb96296cd6d3de1368aa2
Stefan Sperber <stefan.sperber@gmail.com>
2012-10-29 17:23:49 +0000

    Updated licence headers in all source files.

--- src/test/java/fm/last/musicbrainz/data/model/MediumIT.java
+++ src/test/java/fm/last/musicbrainz/data/model/MediumIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Last.fm
+ * Copyright 2012 The musicbrainz-data Authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
